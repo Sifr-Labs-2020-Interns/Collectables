a7a2f7a14a736ce3eafb8cb3d8c496bc0635c77d
Alister Luiz <alisterluiz@gmail.com>
2020-06-30 16:33:38 +0400

    Index file updated

---
+++ lib/utilities/index.dart
@@ -0,0 +1,80 @@
+// Packages
+export 'package:flutter/foundation.dart';
+export 'package:flutter/material.dart';
+export 'package:flutter/services.dart';
+export 'package:font_awesome_flutter/font_awesome_flutter.dart';
+export 'package:carousel_slider/carousel_slider.dart';
+export 'package:auto_size_text/auto_size_text.dart';
+export 'dart:io';
+export 'package:path_provider/path_provider.dart';
+export 'package:url_launcher/url_launcher.dart';
+export 'package:universal_platform/universal_platform.dart';
+export 'package:cloud_firestore/cloud_firestore.dart';
+
+// PDF Support
+export 'package:flutter_full_pdf_viewer/flutter_full_pdf_viewer.dart';
+export 'package:flutter_full_pdf_viewer/full_pdf_viewer_scaffold.dart';
+
+// Firebase
+export 'package:firebase_storage/firebase_storage.dart';
+
+// Models
+export 'package:portfolio_website/models/testimonialsModel.dart';
+export 'package:portfolio_website/models/blogModel.dart';
+export 'package:portfolio_website/models/themeModel.dart';
+export 'package:portfolio_website/models/homeViewModel.dart';
+export 'package:portfolio_website/models/projectModel.dart';
+
+//Utilities
+export 'package:portfolio_website/utilities/elementsLength.dart';
+export 'package:portfolio_website/utilities/routes.dart';
+export 'package:portfolio_website/utilities/screenSize.dart';
+export 'package:portfolio_website/utilities/constants.dart';
+export 'package:portfolio_website/utilities/uiUtilities.dart';
+export 'package:portfolio_website/utilities/theme.dart';
+export 'package:portfolio_website/utilities/pdf_viewer/pdfScreen.dart';
+
+//Widgets
+export 'package:portfolio_website/widgets/primaryButton.dart';
+export 'package:portfolio_website/widgets/baseWidget.dart';
+export 'package:portfolio_website/widgets/common/welcomePageWidgets.dart';
+export 'package:portfolio_website/widgets/common/blogsPageWidgets.dart';
+export 'package:portfolio_website/widgets/common/projectsPageWidgets.dart';
+export 'package:portfolio_website/widgets/common/testimonialsPageWidgets.dart';
+export 'package:portfolio_website/widgets/common/aboutPageWidgets.dart';
+export 'package:portfolio_website/widgets/urlLauncher.dart';
+
+// Responsivea
+export 'package:portfolio_website/enums/device_screen_type.dart';
+export 'package:portfolio_website/responsive/responsive_builder.dart';
+export 'package:portfolio_website/responsive/sizing_information.dart';
+export 'package:portfolio_website/responsive/orientation_layout.dart';
+export 'package:portfolio_website/responsive/screen_type_layout.dart';
+
+// HomeView
+export 'package:portfolio_website/screens/homeView.dart';
+
+// Landscape for Tablet and Laptop
+export 'package:portfolio_website/screens/Landscape/navigationBarLandscape.dart';
+export 'package:portfolio_website/screens/Landscape/welcomePageLandscape.dart';
+export 'package:portfolio_website/screens/Landscape/BlogPageLandscape.dart';
+export 'package:portfolio_website/screens/Landscape/projectsPageLandscape.dart';
+export 'package:portfolio_website/screens/Landscape/TestimonialsPageLandscape.dart';
+export 'package:portfolio_website/screens/Landscape/AboutPageLandscape.dart';
+
+// Portrait for Tablet and Phone
+export 'package:portfolio_website/screens/Portrait/navigationBarPortrait.dart';
+export 'package:portfolio_website/screens/Portrait/welcomePagePortrait.dart';
+export 'package:portfolio_website/screens/Portrait/projectsPagePortrait.dart';
+export 'package:portfolio_website/screens/Portrait/blogsPagePortrait.dart';
+export 'package:portfolio_website/screens/Portrait/TestimonialsPagePortrait.dart';
+export 'package:portfolio_website/screens/Portrait/aboutPagePortrait.dart';
+
+// Services
+export 'package:portfolio_website/services/databaseService.dart';
+
+// Providers
+export 'package:provider/provider.dart';
+export 'package:portfolio_website/providers/currentProjectIDProvider.dart';
+export 'package:portfolio_website/providers/currentPageProvider.dart';
+export 'package:portfolio_website/providers/pdfProviders.dart';
