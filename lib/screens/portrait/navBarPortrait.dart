43366c196db1c70f47ea16005f8e80c91ff6f0c7
Alister Luiz <alisterluiz@gmail.com>
2020-06-30 16:47:23 +0400

    Navigation Bar Portrait Updated

---
+++ lib/screens/portrait/navBarPortrait.dart
@@ -0,0 +1,10 @@
+import 'package:Collectables/utilities/index.dart';
+
+class NavigationBarPortrait extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
