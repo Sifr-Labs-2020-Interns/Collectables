4bd3a2ab9eb62d7a0552827fed64ab126f27550e
Alister Luiz <alisterluiz@gmail.com>
2020-06-30 16:41:22 +0400

    Index files cleaned

--- lib/utilities/index.dart
+++ lib/utilities/index.dart
@@ -6,43 +6,21 @@ export 'package:font_awesome_flutter/font_awesome_flutter.dart';
 export 'package:carousel_slider/carousel_slider.dart';
 export 'package:auto_size_text/auto_size_text.dart';
 export 'dart:io';
-export 'package:path_provider/path_provider.dart';
-export 'package:url_launcher/url_launcher.dart';
-export 'package:universal_platform/universal_platform.dart';
-export 'package:cloud_firestore/cloud_firestore.dart';
-
-// PDF Support
-export 'package:flutter_full_pdf_viewer/flutter_full_pdf_viewer.dart';
-export 'package:flutter_full_pdf_viewer/full_pdf_viewer_scaffold.dart';
 
 // Firebase
-export 'package:firebase_storage/firebase_storage.dart';
 
 // Models
-export 'package:Collectables/models/testimonialsModel.dart';
-export 'package:Collectables/models/blogModel.dart';
 export 'package:Collectables/models/themeModel.dart';
-export 'package:Collectables/models/homeViewModel.dart';
-export 'package:Collectables/models/projectModel.dart';
 
 //Utilities
-export 'package:Collectables/utilities/elementsLength.dart';
 export 'package:Collectables/utilities/routes.dart';
 export 'package:Collectables/utilities/screenSize.dart';
 export 'package:Collectables/utilities/constants.dart';
 export 'package:Collectables/utilities/uiUtilities.dart';
 export 'package:Collectables/utilities/theme.dart';
-export 'package:Collectables/utilities/pdf_viewer/pdfScreen.dart';
 
 //Widgets
-export 'package:Collectables/widgets/primaryButton.dart';
 export 'package:Collectables/widgets/baseWidget.dart';
-export 'package:Collectables/widgets/common/welcomePageWidgets.dart';
-export 'package:Collectables/widgets/common/blogsPageWidgets.dart';
-export 'package:Collectables/widgets/common/projectsPageWidgets.dart';
-export 'package:Collectables/widgets/common/testimonialsPageWidgets.dart';
-export 'package:Collectables/widgets/common/aboutPageWidgets.dart';
-export 'package:Collectables/widgets/urlLauncher.dart';
 
 // Responsivea
 export 'package:Collectables/enums/device_screen_type.dart';
@@ -55,26 +33,10 @@ export 'package:Collectables/responsive/screen_type_layout.dart';
 export 'package:Collectables/screens/homeView.dart';
 
 // Landscape for Tablet and Laptop
-export 'package:Collectables/screens/Landscape/navigationBarLandscape.dart';
-export 'package:Collectables/screens/Landscape/welcomePageLandscape.dart';
-export 'package:Collectables/screens/Landscape/BlogPageLandscape.dart';
-export 'package:Collectables/screens/Landscape/projectsPageLandscape.dart';
-export 'package:Collectables/screens/Landscape/TestimonialsPageLandscape.dart';
-export 'package:Collectables/screens/Landscape/AboutPageLandscape.dart';
 
 // Portrait for Tablet and Phone
-export 'package:Collectables/screens/Portrait/navigationBarPortrait.dart';
-export 'package:Collectables/screens/Portrait/welcomePagePortrait.dart';
-export 'package:Collectables/screens/Portrait/projectsPagePortrait.dart';
-export 'package:Collectables/screens/Portrait/blogsPagePortrait.dart';
-export 'package:Collectables/screens/Portrait/TestimonialsPagePortrait.dart';
-export 'package:Collectables/screens/Portrait/aboutPagePortrait.dart';
 
 // Services
-export 'package:Collectables/services/databaseService.dart';
 
 // Providers
 export 'package:provider/provider.dart';
-export 'package:Collectables/providers/currentProjectIDProvider.dart';
-export 'package:Collectables/providers/currentPageProvider.dart';
-export 'package:Collectables/providers/pdfProviders.dart';
