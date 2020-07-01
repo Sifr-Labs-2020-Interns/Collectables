7601442b7f83f4ca1a3a8737495a0126d9ebff7f
Alister Luiz <alisterluiz@gmail.com>
2020-07-01 20:04:00 +0400

    NavBar Provider Added

--- lib/screens/portrait/registration/otpScreenPortrait.dart
+++ lib/screens/portrait/registration/otpScreenPortrait.dart
@@ -14,90 +14,92 @@ class _OTPScreenPortraitState extends State<OTPScreenPortrait> {
   Widget build(BuildContext context) {
     TextEditingController controller = TextEditingController();
 
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: screenHeight(context),
-          padding: EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 20,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              getRegistrationHeader(3, 'Verify OTP', () {
-                Navigator.pop(context);
-              }),
-              Text('A verification code has been sent to'),
-              Text(
-                'test@gmail.com',
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  color: Theme.of(context).accentColor,
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            height: screenHeight(context),
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 20,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                getRegistrationHeader(3, 'Verify OTP', () {
+                  Navigator.pop(context);
+                }),
+                Text('A verification code has been sent to'),
+                Text(
+                  'test@gmail.com',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    color: Theme.of(context).accentColor,
+                  ),
                 ),
-              ),
-              Expanded(
-                flex: 4,
-                child: Column(
-                  children: [
-                    PinCodeTextField(
-                      autofocus: true,
-                      controller: controller,
-                      hideCharacter: false,
-                      highlight: true,
-                      highlightColor: Theme.of(context).accentColor,
-                      defaultBorderColor: Theme.of(context).primaryColor,
-                      hasTextBorderColor: Theme.of(context).primaryColor,
-                      maxLength: pinLength,
-                      hasError: hasError,
-                      maskCharacter: "*",
-                      onTextChanged: (text) {
-                        setState(() {
-                          hasError = false;
-                        });
-                      },
-                      onDone: (text) {
-                        pin = text;
-                      },
-                      wrapAlignment: WrapAlignment.start,
-                      pinBoxDecoration:
-                          ProvidedPinBoxDecoration.underlinedPinBoxDecoration,
-                      pinTextStyle: TextStyle(
-                        fontWeight: FontWeight.w800,
-                        fontSize: 32,
+                Expanded(
+                  flex: 4,
+                  child: Column(
+                    children: [
+                      PinCodeTextField(
+                        autofocus: true,
+                        controller: controller,
+                        hideCharacter: false,
+                        highlight: true,
+                        highlightColor: Theme.of(context).accentColor,
+                        defaultBorderColor: Theme.of(context).primaryColor,
+                        hasTextBorderColor: Theme.of(context).primaryColor,
+                        maxLength: pinLength,
+                        hasError: hasError,
+                        maskCharacter: "*",
+                        onTextChanged: (text) {
+                          setState(() {
+                            hasError = false;
+                          });
+                        },
+                        onDone: (text) {
+                          pin = text;
+                        },
+                        wrapAlignment: WrapAlignment.start,
+                        pinBoxDecoration:
+                            ProvidedPinBoxDecoration.underlinedPinBoxDecoration,
+                        pinTextStyle: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 32,
+                        ),
+                        pinTextAnimatedSwitcherTransition:
+                            ProvidedPinBoxTextAnimation.scalingTransition,
+                        pinTextAnimatedSwitcherDuration:
+                            Duration(milliseconds: 300),
                       ),
-                      pinTextAnimatedSwitcherTransition:
-                          ProvidedPinBoxTextAnimation.scalingTransition,
-                      pinTextAnimatedSwitcherDuration:
-                          Duration(milliseconds: 300),
-                    ),
-                    sizedBox(30, 0),
-                    getButton(context, 'VERIFY NOW', () {}),
-                    sizedBox(30, 0),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Did not recieve a code?'),
-                          sizedBox(0, 5),
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.pushNamed(
-                                  context, Routes.signUpPortrait);
-                            },
-                            child: Text(
-                              'Resend Email',
-                              style: TextStyle(
-                                  color: Theme.of(context).accentColor),
+                      sizedBox(30, 0),
+                      getButton(context, 'VERIFY NOW', () {}),
+                      sizedBox(30, 0),
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Did not recieve a code?'),
+                            sizedBox(0, 5),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.pushNamed(
+                                    context, Routes.signUpPortrait);
+                              },
+                              child: Text(
+                                'Resend Email',
+                                style: TextStyle(
+                                    color: Theme.of(context).accentColor),
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/portrait/registration/signUpPortrait.dart
+++ lib/screens/portrait/registration/signUpPortrait.dart
@@ -11,83 +11,85 @@ class _SignUpPortraitState extends State<SignUpPortrait> {
   TextEditingController passwordController = new TextEditingController();
 
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      body: SingleChildScrollView(
-        child: Container(
-          height: screenHeight(context),
-          padding: EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 20,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              getRegistrationHeader(2, 'Sign Up', () {
-                Navigator.pop(context);
-              }),
-              Expanded(
-                flex: 3,
-                child: Column(
-                  children: [
-                    getTextField(context, 'Name', nameController,
-                        TextInputType.emailAddress),
-                    sizedBox(15, 0),
-                    getTextField(context, 'Email', emailController,
-                        TextInputType.emailAddress),
-                    sizedBox(15, 0),
-                    getTextField(context, 'Password', passwordController,
-                        TextInputType.visiblePassword),
-                    sizedBox(15, 0),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text('Already have an account?'),
-                          sizedBox(0, 5),
-                          Icon(
-                            FontAwesomeIcons.longArrowAltRight,
-                            color: Theme.of(context).accentColor,
-                          ),
-                        ],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        body: SingleChildScrollView(
+          child: Container(
+            height: screenHeight(context),
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 20,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                getRegistrationHeader(2, 'Sign Up', () {
+                  Navigator.pop(context);
+                }),
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: [
+                      getTextField(context, 'Name', nameController,
+                          TextInputType.emailAddress),
+                      sizedBox(15, 0),
+                      getTextField(context, 'Email', emailController,
+                          TextInputType.emailAddress),
+                      sizedBox(15, 0),
+                      getTextField(context, 'Password', passwordController,
+                          TextInputType.visiblePassword),
+                      sizedBox(15, 0),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text('Already have an account?'),
+                            sizedBox(0, 5),
+                            Icon(
+                              FontAwesomeIcons.longArrowAltRight,
+                              color: Theme.of(context).accentColor,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    sizedBox(15, 0),
-                    getButton(
+                      sizedBox(15, 0),
+                      getButton(
+                        context,
+                        'SIGN UP',
+                        () {
+                          Navigator.pushNamed(
+                            context,
+                            Routes.otpScreenPortrait,
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Center(
+                  child: Text('Or sign up with social account'),
+                ),
+                sizedBox(15, 0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    getRegistrationIcons(
                       context,
-                      'SIGN UP',
-                      () {
-                        Navigator.pushNamed(
-                          context,
-                          Routes.otpScreenPortrait,
-                        );
-                      },
+                      'assets/images/google.png',
+                    ),
+                    sizedBox(0, 30),
+                    getRegistrationIcons(
+                      context,
+                      'assets/images/facebook.png',
                     ),
                   ],
                 ),
-              ),
-              Center(
-                child: Text('Or sign up with social account'),
-              ),
-              sizedBox(15, 0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  getRegistrationIcons(
-                    context,
-                    'assets/images/google.png',
-                  ),
-                  sizedBox(0, 30),
-                  getRegistrationIcons(
-                    context,
-                    'assets/images/facebook.png',
-                  ),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
