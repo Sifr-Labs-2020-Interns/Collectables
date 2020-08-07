cdc8947093a4e84c479afa35b5ac58eaf8232800
Alister Luiz <alisterluiz@gmail.com>
2020-08-07 21:34:37 +0400

    Categories Page Filters Completed

--- lib/screens/portrait/shop/itemsPage.dart
+++ lib/screens/portrait/shop/itemsPage.dart
@@ -8,6 +8,7 @@ class ItemsPagePortrait extends StatefulWidget {
 class _ItemsPagePortraitState extends State<ItemsPagePortrait> {
   String searchText = '';
   bool onSaved = false;
+  bool grid = true;
   TextEditingController searchController = TextEditingController();
   FocusNode myFocusNode;
 
@@ -47,15 +48,18 @@ class _ItemsPagePortraitState extends State<ItemsPagePortrait> {
                     right: 14.0,
                   ),
                   child: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          onSaved = false;
-                          searchController.clear();
-                          searchText = '';
-                          myFocusNode.requestFocus();
-                        });
-                      },
-                      icon: Icon(Icons.search)),
+                    onPressed: () {
+                      setState(() {
+                        onSaved = false;
+                        searchController.clear();
+                        searchText = '';
+                        myFocusNode.requestFocus();
+                      });
+                    },
+                    icon: Icon(
+                      Icons.search,
+                    ),
+                  ),
                 )
               : Padding(
                   padding: EdgeInsets.only(
@@ -75,44 +79,80 @@ class _ItemsPagePortraitState extends State<ItemsPagePortrait> {
                 ),
         ],
         bottom: PreferredSize(
+          preferredSize: Size.fromHeight(
+            screenHeight(context) * 0.13,
+          ),
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 28.0),
+            padding: EdgeInsets.symmetric(horizontal: 14.0),
             child: Column(
               children: [
-                TextField(
-                  focusNode: myFocusNode,
-                  controller: searchController,
-                  autofocus: true,
-                  cursorColor: Theme.of(context).primaryColor,
-                  decoration: new InputDecoration(
-                    border: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    errorBorder: InputBorder.none,
-                    disabledBorder: InputBorder.none,
+                Padding(
+                  padding: EdgeInsets.only(left: 14.0),
+                  child: TextField(
+                    focusNode: myFocusNode,
+                    controller: searchController,
+                    autofocus: true,
+                    cursorColor: Theme.of(context).primaryColor,
+                    decoration: new InputDecoration(
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      errorBorder: InputBorder.none,
+                      disabledBorder: InputBorder.none,
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        searchText = value;
+                      });
+                    },
+                    onSubmitted: (value) {
+                      setState(() {
+                        onSaved = true;
+                      });
+                    },
+                    style: TextStyle(
+                      fontSize: 34,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                  onChanged: (value) {
-                    setState(() {
-                      searchText = value;
-                    });
-                  },
-                  onSubmitted: (value) {
-                    setState(() {
-                      onSaved = true;
-                    });
-                  },
-                  style: TextStyle(
-                    fontSize: 34,
-                    fontWeight: FontWeight.bold,
-                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    getSearchFilters(Icons.filter_list, 'Filters', () {}),
+                    getSearchFilters(
+                        Icons.compare_arrows, 'Price: Low to High', () {}),
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          grid = !grid;
+                        });
+                      },
+                      icon: (grid == true)
+                          ? Icon(Icons.grid_on)
+                          : Icon(Icons.list),
+                    ),
+                  ],
                 ),
               ],
             ),
           ),
-          preferredSize: Size.fromHeight(
-            screenHeight(context) * 0.08,
-          ),
         ),
+      ),
+    );
+  }
+
+  InkWell getSearchFilters(IconData icon, String text, Function onTap) {
+    return InkWell(
+      onTap: onTap,
+      child: Row(
+        children: [
+          Icon(
+            icon,
+          ),
+          sizedBox(0, 2),
+          Text(text),
+        ],
       ),
     );
   }
